ab5d015d823ef2f343cb9e06bc7988fc1893d6a2
Sanganak Abhiyanta <imsanganakabhiyanta@gmail.com>
2022-01-01 19:26:28 +0530

    first question first day solved

---
+++ day1/printAnagramTogenther.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        vector<vector<string>> result;
+        unordered_map<string,vector<string>> u_map;
+        
+        for(auto str:string_list)
+        {
+            string strr=str;
+            sort(strr.begin(),strr.end());
+            u_map[strr].push_back(str);
+        }
+        
+        for(auto it:u_map)
+        {
+            result.push_back(it.second);
+        }
+        
+        return result;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
